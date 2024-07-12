e03d9f418e92dc930747634cb01d3543941b29ee
Walter Sanchez <sanchez.walter@gmail.com>
2024-07-12 14:28:09 -0300

    Modifica el html para que cargue informacion desde la bd

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,7 @@
-document.getElementById('formBuscar').addEventListener('submit',(e)=>{
-    e.preventDefault()
-    buscarLibros()
-})
-const  buscarLibros =()=> {
+document.addEventListener('DOMContentLoaded', function (e) {
 
-    const query = document.getElementById('inputBuscar').value;
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&langRestrict=es`)
+
+    fetch(`http://localhost:8080/books/libros`)
         .then(response => response.json())
         .then(data => {
             mostrarResultados(data);
@@ -13,32 +9,35 @@ const  buscarLibros =()=> {
         .catch(error => {
             console.error('Error:', error);
         });
-}
 
-const mostrarResultados= (data) => {
+
+})
+
+
+const mostrarResultados = (data) => {
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
 
-    if (data.items) {
-        data.items.forEach(item => {
-            const libro = item.volumeInfo;
-            let imageUrl = '';
-            if (libro.imageLinks) {
-                imageUrl = libro.imageLinks.extraLarge || libro.imageLinks.large || libro.imageLinks.medium || libro.imageLinks.small || libro.imageLinks.thumbnail;
-            }
+    if (data) {
+        data.forEach(item => {
+            const libro = item;
+            // let imageUrl = '';
+            // if (libro.imageLinks) {
+            //     imageUrl = libro.imageLinks.extraLarge || libro.imageLinks.large || libro.imageLinks.medium || libro.imageLinks.small || libro.imageLinks.thumbnail;
+            // }
             const libroElement = document.createElement('div');
-            libroElement.className+="col-12 col-md-6 col-lg-4 d-flex justify-content-center"
-            libroElement.innerHTML=`
+            libroElement.className += "col-12 col-md-6 col-lg-4 d-flex justify-content-center"
+            libroElement.innerHTML = `
                 <div class="card">
                     <div class="card__side card__side--front-1">
-                        ${libro.imageLinks ? `<img src="${libro.imageLinks.thumbnail}" alt="Portada de ${libro.title}">` : 'No hay imagen disponible'}
+                        ${libro.imageLinks ? `<img src="${libro.imageLinks.thumbnail}" alt="Portada de ${libro.titulo}">` : 'No hay imagen disponible'}
                     </div>
                     <div class="card__side card__side--back card__side--back-1">
                         <div class="text">
                             <a href="detalle.html">
-                                <h4>${libro.title}</h4>
+                                <h4>${libro.titulo}</h4>
                                 <p class="titulo">
-                                    ${libro.description ? libro.description : 'No disponible'}
+                                    ${libro.descripcion ? libro.descripcion : 'No disponible'}
                                 </p>
                             </a>
                         </div>     
@@ -51,27 +50,3 @@ const mostrarResultados= (data) => {
         resultsDiv.innerHTML = '<p>No se encontraron resultados</p>';
     }
 }
-/*
-<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-              <div class="card">
-                <div class="card__side card__side--front-1">
-    
-                  <img src="./assets/img/portada-libro-01.webp" alt="Libre como el viento" loading="lazy">
-    
-                </div>
-                <div class="card__side card__side--back card__side--back-1">
-                  <div class="text">
-                    <a href="detalle.html">
-                      <h4>«Libre como el viento»</h4>
-                      <p class="titulo">
-                        Lady Johanna McRae, hija de lady Megan y el laird Duncan McRae, es una joven intrépida que, junto
-                        con su hermana Amanda, trae a su padre de cabeza. <br>
-                        Si algo le gusta a Johanna es disfrutar de su libertad ayudando a quien puede y, en especial, retar
-                        a todo aquel que se enfrente a ella.
-                      </p>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-*/
